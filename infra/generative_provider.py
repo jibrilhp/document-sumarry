88f42d879f84e7e0b253eba188896eaf7699db35
Ekky <ekharisma@outlook.com>
2025-08-10 11:08:25 +0700

    add rate limit for gemini api call; add retry mechanism for gemini api call

--- infra/generative_provider.py
+++ infra/generative_provider.py
@@ -1,10 +1,10 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 from langchain_ollama.llms import OllamaLLM
 from langchain_ollama.embeddings import OllamaEmbeddings
 from infra.settings import Settings
 import logging
+from langchain_core.rate_limiters import InMemoryRateLimiter
 
 
 class GenerativeAdapter:
@@ -23,8 +23,17 @@ class GenerativeAdapter:
             )
             logging.info(f"using ollama provider with base URL: {settings.OLLAMA_BASE_URL}.  chat model: {settings.CHAT_MODEL_NAME}, embedding model: {settings.EMBEDDING_MODEL_NAME}")
         elif settings.LLM_PROVIDER == "google":
+            rate_limiter = InMemoryRateLimiter(
+                requests_per_second=0.5,
+                check_every_n_seconds=0.1,
+                max_bucket_size=10,
+            )
             self.embedding_model = GoogleGenerativeAIEmbeddings(model=settings.EMBEDDING_MODEL_NAME)
-            self.chat_model = ChatGoogleGenerativeAI(model=settings.CHAT_MODEL_NAME)
+            self.chat_model = ChatGoogleGenerativeAI(
+                model=settings.CHAT_MODEL_NAME,
+                rate_limiter=rate_limiter,
+                max_retries=3
+            )
             logging.info(f"using google generative provider")
         elif settings.LLM_PROVIDER == "ollama_cpu":
             self.embedding_model = OllamaEmbeddings(
