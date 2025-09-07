f25d5c74e116d64575832fdd3788f117e10928bd
Jibril Hartri Putra <jibril.hartri@bluebirdgroup.com>
2025-09-07 08:20:02 +0700

    add internal path

--- handler/routes.py
+++ handler/routes.py
@@ -455,6 +455,37 @@ class Routes:
                 self.logger.error(str(e))
                 raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "please try again later")
 
+        @self.app.get("/v2/internal/conversation/{conversation_uuid}")
+        async def internal_chat_history_v2(
+            tenant_id: Annotated[str | None, Header()],
+            api_key: Annotated[str,  Header()],
+            conversation_uuid: str,
+            username: Annotated[str | None, Header()] = None,
+        ) -> List[ConversationStateV2]:
+            try:
+                if username is None:
+                    username = ""
+                    self.user_usecase.get_api_key_internal(api_key=api_key)
+                else:
+                    self.user_usecase.get_api_key(username=username, api_key=api_key)
+                conversation = Conversation(tenant_id=tenant_id, conversation_uuid=conversation_uuid)
+                return self.conversation_usecase.get_chat_history_v2(conversation)
+            except UnauthorizedAccess as e:
+                self.logger.error(str(e))
+                raise HTTPException(status.HTTP_403_FORBIDDEN, "unauthorized access, please provide valid API key")
+            except ResourceNotFound as e:
+                self.logger.error(str(e))
+                raise HTTPException(status.HTTP_404_NOT_FOUND, str(e))
+            except FileTooLarge as e:
+                self.logger.error(str(e))
+                raise HTTPException(status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, str(e))
+            except UnknownFileType as e:
+                self.logger.error(str(e))
+                raise HTTPException(status.HTTP_400_BAD_REQUEST, str(e))
+            except Exception as e:
+                self.logger.error(str(e))
+                raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "please try again later")
+            
         @self.app.post("/v1/xai")
         async def upload_file_xai(
             request: Request,
